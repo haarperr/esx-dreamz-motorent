a69d83c97ecf3413d484b22098f465512bb00ed0
ツ DREAMZ <42575370+DREAMZOFC@users.noreply.github.com>
2019-06-13 20:54:04 +0100

    Add files via upload

---
+++ translations/en.lua
@@ -0,0 +1,20 @@
+Locales['en'] = {
+	['press_e'] = 'Press ~INPUT_CONTEXT~ to rent a moto.',
+	['storebike'] = 'Press ~INPUT_CONTEXT~ to return a moto.',	
+	['biketitle'] = 'Moto Rental',
+	['bike'] = 'Moto - <span style="color:green;">Maibatsu Sanchez</span> <span style="color:red;">100$</span>',
+	['bike2'] = 'Moto - <span style="color:green;">Dinka Akuma</span> <span style="color:red;">100$</span>',
+	['bike3'] = 'Moto - <span style="color:green;">Pegassi Bati 800</span> <span style="color:red;">100$</span>',
+	['bike4'] = 'Moto - <span style="color:green;">Pegassi Ruffian</span> <span style="color:red;">100$</span>',
+	['bikefree'] = 'Moto - <span style="color:green;">Maibatsu Sanchez</span>',
+	['bike2free'] = 'Moto - <span style="color:green;">Dinka Akuma</span>',
+	['bike3free'] = 'Moto - <span style="color:green;">Pegassi Bati 800</span>',
+	['bike4free'] = 'Moto - <span style="color:green;">Pegassi Ruffian</span>',
+	['bikemessage'] = 'We hope you have enjoyed the rental. Come back more often.',
+	['notabike'] = 'You are not on the moto!',
+	
+	['bike_pay'] = 'You Paid: $%s',
+	
+	['bikes'] = '[Moto Rental]',
+	
+}
---
+++ translations/pt.lua
@@ -0,0 +1,20 @@
+Locales['pt'] = {
+	['press_e'] = 'Pressione ~INPUT_CONTEXT~ para alugar uma moto.',
+	['storebike'] = 'Pressione ~INPUT_CONTEXT~ para devolver a moto.',
+	['biketitle'] = 'Aluguer de Moto',
+	['bike'] = 'Moto - <span style="color:green;">Maibatsu Sanchez</span> <span style="color:red;">100€</span>',
+	['bike2'] = 'Moto - <span style="color:green;">Dinka Akuma</span> <span style="color:red;">100€</span>',
+	['bike3'] = 'Moto - <span style="color:green;">Pegassi Bati 800</span> <span style="color:red;">120€</span>',
+	['bike4'] = 'Moto - <span style="color:green;">Pegassi Ruffian</span> <span style="color:red;">100€</span>',
+	['bikefree'] = 'Moto - <span style="color:green;">Maibatsu Sanchez</span>',
+	['bike2free'] = 'Moto - <span style="color:green;">Dinka Akuma</span>',
+	['bike3free'] = 'Moto - <span style="color:green;">Pegassi Bati 800</span>',
+	['bike4free'] = 'Moto - <span style="color:green;">Pegassi Ruffian</span>',
+	['bikemessage'] = 'Esperamos que você tenha gostado do passeio. Volte mais vezes.',
+	['notabike'] = 'Você não está na moto!',
+	
+	['bike_pay'] = 'Você pagou: €%s',
+	
+	['bikes'] = '[Aluguer de Motos]',
+	
+}
